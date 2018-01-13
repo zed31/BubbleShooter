4e21b654eb4dcb6aff7b5223e633a85603f3ab28
Zed <clement.francois@epitech.eu>
2018-01-13 16:03:49 +0100

    BubbleMap: ADD: Generator of bubble

---
+++ BubbleShooter/Assets/Scripts/BubbleMap.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using BubbleShooter;
+using UnityEngine;
+
+public class BubbleMap : MonoBehaviour {
+
+    [SerializeField] private GameObject m_gameObjectRef;
+
+    private ComponentContainer m_bubbles = new ComponentContainer();
+    private float m_startX = -4.5f;
+    private float m_startY = 4.41f;
+    private float m_scale = 0.60f;
+
+	// Use this for initialization
+	void Awake() {
+        foreach (var i in Enumerable.Range(0, 14)) {
+            foreach (var j in Enumerable.Range(0, 16)) {
+                m_bubbles.Push(m_startX, m_startY, m_gameObjectRef);
+                m_startX += m_scale;
+            }
+            m_startX = -4.5f;
+            m_startY -= m_scale;
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

73984ce69373b3171fa547c33379befb3a45b924
Zed <clement.francois@epitech.eu>
2018-01-13 10:29:58 +0100

    ArrowController: ADD: Script to control the arrow

---
+++ BubbleShooter/Assets/Scripts/ArrowController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+
+    [SerializeField] private float m_speed = 4f;
+    private Rigidbody2D m_rigidbody;
+    private float m_movement = 0f;
+	
+    void Start() {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        m_movement = Input.GetAxisRaw("Horizontal") * m_speed;
+    }
+
+    void FixedUpdate() {
+        m_rigidbody.MoveRotation(m_rigidbody.rotation + m_movement * Time.fixedDeltaTime);
+    }
+}

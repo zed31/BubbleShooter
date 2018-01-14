844e2e6d1350223ac89402ea4b86549959ba1352
Zed <clement.francois@epitech.eu>
2018-01-14 18:30:03 +0100

    BubbleShoot: ADD: Script to the shooted bubble to increase its velocity

---
+++ BubbleShooter/Assets/Scripts/BubbleShoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BubbleShoot : MonoBehaviour {
+
+    private Rigidbody2D m_rigidbody2D;
+    private float m_speed = 50f;
+
+    public void Normalize()
+    {
+        m_rigidbody2D.velocity = m_rigidbody2D.velocity.normalized * m_speed;
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        m_rigidbody2D.velocity = direction;
+        Normalize();
+    }
+
+    void Awake()
+    {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Normalize();
+    }
+}

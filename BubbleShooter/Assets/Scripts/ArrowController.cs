95e97617582407769e8ae601300787e59df5903d
Zed <clement.francois@epitech.eu>
2018-01-14 18:25:03 +0100

    ArrowController: REFACTOR: Remove rigid body and change the rigid body rotation to the rotate

--- BubbleShooter/Assets/Scripts/ArrowController.cs
+++ BubbleShooter/Assets/Scripts/ArrowController.cs
@@ -4,32 +4,29 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour {
 
+    public static bool IsFireing = false;
     [SerializeField] private float m_speed = 4f;
+    [SerializeField] private FirePointController m_controller;
 
-    private Rigidbody2D m_rigidbody;
     private float m_movement = 0f;
 	
-    public float GetAngle
-    {
-        get
-        {
-            return transform.rotation.eulerAngles.z;
-        }
-    }
-
     void Start() {
         GetComponent<SpriteRenderer>().material.color = ComponentColorGenerator.Instance.GetColor();
-        m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetButton("Shoot") && !IsFireing)
+        {
+            IsFireing = true;
+            m_controller.Shoot();
+        }
         m_movement = Input.GetAxisRaw("Horizontal") * m_speed;
     }
 
     void FixedUpdate() {
-        if (Math.Abs(m_rigidbody.rotation + m_movement * Time.fixedDeltaTime) < 75f) {
-            m_rigidbody.MoveRotation(m_rigidbody.rotation + m_movement * Time.fixedDeltaTime);
+        if (Math.Abs(m_movement * Time.fixedDeltaTime) < 75f) {
+            transform.Rotate(new Vector3(0f, 0f, m_movement * Time.fixedDeltaTime));
         }
     }
 }

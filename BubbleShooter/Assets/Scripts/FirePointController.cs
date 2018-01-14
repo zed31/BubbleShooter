824e8fa7fc347b8bd9cf43262cad7e623590204e
Zed <clement.francois@epitech.eu>
2018-01-14 18:30:21 +0100

    FirePoint: ADD: FirePoint to generate the Shooted bubble

---
+++ BubbleShooter/Assets/Scripts/FirePointController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FirePointController : MonoBehaviour {
+
+    [SerializeField] private GameObject m_bulletRef;
+    private Color32 m_color;
+
+    public void SetColor(Color32 color)
+    {
+        m_color = color;
+    }
+
+    public void Shoot()
+    {
+        GameObject Bubble = Instantiate(m_bulletRef, transform.position, transform.rotation);
+        Bubble.GetComponent<SpriteRenderer>().material.color = m_color;
+        Bubble.GetComponent<BubbleShoot>().SetDirection(new Vector2(transform.position.x, 1f).normalized);
+    }
+}
